1c49d0eaf7d51c96a683ec5301551f4807ec3b86
Jack Pincus <jwpincus@gmail.com>
2019-02-18 12:15:54 -0800

    add styling to calculator

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -22,14 +22,17 @@ class Calculator extends React.Component {
         newState[name] = value;
         this.setState({
             ...newState
-        })
-    }
+        });
+    };
+
+
 
     render () {
         
        return ( 
-            <div className={classes.Calculator}>
-                <label htmlFor="currentTax">Your current marginal tax rate, represented as a percentage. This is also known as your tax bracket. Remember to combine provincial and federal taxes.</label>
+            <div className={classes.CalculatorInputs}>
+                <h2>Savings return calculator</h2>
+                <label htmlFor="currentTax">What is your current marginal tax rate, represented as a percentage. <br/>This is also known as your tax bracket. Remember to combine provincial and federal taxes.</label>
                 <input 
                     type="number" 
                     name="currentTax"
@@ -37,7 +40,7 @@ class Calculator extends React.Component {
                     onChange={this.inputChangeHandler}
                     value={this.state.currentTax}/>
 
-                <label htmlFor="futureTax">Your anticipated future tax rate. This is the marginal tax rate, or tax bracket that you anticipate paying at the time that you start withdrawing your savings.</label>
+                <label htmlFor="futureTax">What is your anticipated future tax rate. <br/> This is the marginal tax rate, or tax bracket that you anticipate paying at the time that you start withdrawing your savings.</label>
                 <input 
                     type="number" 
                     name="futureTax"
@@ -45,7 +48,7 @@ class Calculator extends React.Component {
                     onChange={this.inputChangeHandler}
                     value={this.state.futureTax}/>
 
-                <label htmlFor="principal">What amount are you planning on investing? (input this as an after tax number, we'll do the calculation for your tax refund, and add it to the total!)</label>
+                <label htmlFor="principal">What amount are you planning on investing? <br/> Input this as an after tax number, we'll do the calculation for your tax refund, and add it to the total!</label>
                 <input 
                     type="number" 
                     name="principal"
@@ -53,14 +56,14 @@ class Calculator extends React.Component {
                     onChange={this.inputChangeHandler}
                     value={this.state.principal}/>
                 
-                <label htmlFor="years">How long will you give your money to grow? Just tell us how many years until you plan on withdrawing from your account</label>
+                <label htmlFor="years">How long will you give your money to grow? <br/> Just tell us how many years until you plan on withdrawing from your account</label>
                 <input 
                     type="number" 
                     name="years"
                     onChange={this.inputChangeHandler}
                     value={this.state.years}/>
 
-                <label htmlFor="returnRate">What is the anticipated yearly return rate for your investments as a percentage? (The default we've provided is the 20 year average for the S&P/TSX composite)</label>
+                <label htmlFor="returnRate">What is the anticipated yearly return rate for your investments as a percentage? <br/> The default we've provided is the 20 year average for the S&P/TSX composite</label>
                 <input 
                     type="number" 
                     step= ".01"
@@ -68,7 +71,7 @@ class Calculator extends React.Component {
                     onChange={this.inputChangeHandler}
                     value={this.state.returnRate}/>
 
-                <label htmlFor="inflation">What is the anticipated inflation rate until you start withdrawing funds? (The default we've provided is the 20 year average for the Canadian CPI)</label>
+                <label htmlFor="inflation">What is the anticipated inflation rate until you start withdrawing funds? <br/> The default we've provided is the 20 year average for the Canadian CPI</label>
                 <input 
                     type="number" 
                     step= ".01"
@@ -77,7 +80,7 @@ class Calculator extends React.Component {
                     value={this.state.inflation}/>
             </div>
        )
-    }
-}
+    };
+};
 
 export default Calculator;
